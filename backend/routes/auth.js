7363af200499427ddd69b48a90ade9e010b4437b
MUKUL <mukul.2226csit1115@kiet.edu>
2024-04-04 01:17:37 +0530

    Password Hashing and Json web token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Harryisagood$boy';
 
 //Create a User Using : POST "/api/auth/CreateUser". Doesn't require Auth  //No Login Required
 router.post('/CreateUser', [
@@ -16,18 +19,29 @@ router.post('/CreateUser', [
         return res.status(400).json({ errors: errors.array() })
     }
 
-    //Check whether the user with this email exist already
     try {
+        //Check whether the user with this email exist already
+
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt)
+        //create a user 
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
-        })
-        res.json(user)
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken  = jwt.sign(data, JWT_SECRET);
+        // res.json(user)
+        res.json({ authtoken})
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some Error Occured");
