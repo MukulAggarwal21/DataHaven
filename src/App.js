b5500b02d1c58abaec27a15a44a9e7ff0f94f0d9
MUKUL <mukul.2226csit1115@kiet.edu>
2024-04-09 22:14:10 +0530

    Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is my react notebook</h1>
-    </div>
+    <>
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/about" element={ <About />}>
+            </Route>
+            <Route exact path="/" element = {<Home/>}>
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
